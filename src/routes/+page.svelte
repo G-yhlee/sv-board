92e432f04fcc6f7f3b67741322d25c9f193aad1e
G-yhlee <yhleesoft@gmail.com>
2023-03-13 11:42:31 +0900

    === board game ===

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import App from "../game/App.svelte";
+
+
+    
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<App></App>
