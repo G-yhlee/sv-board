33cc865f7acbe8c718057d8a71d0d021adfe7a64
G-yhlee <yhleesoft@gmail.com>
2023-03-13 17:43:14 +0900

    add ts-pattern

---
+++ src/game_x1/store/onPlayer.ts
@@ -0,0 +1,17 @@
+import { match,P } from 'ts-pattern';
+// import logger from '$lib/fucntion/logger';
+
+import {S_onPlayer} from './store'
+
+export function onPlayer(actionPattern:any) {
+    // logger(actionPattern)
+    // let {pageLength, pageCurrent} = actionPattern
+
+    // let eventResponse = match({...actionPattern, isEnd: (pageLength === pageCurrent), isStart: (pageCurrent===1) })
+    // .with({ type: 'increase', isEnd: false  } , (res)=> {
+    //     S_onPage.update(s=> Number(s+1))
+    //     S_onPageData.update(s=> res)
+    //     return {...res,msg: 'page right'}
+    // })
+    // eventResponse.run()
+}
---
+++ src/game_x1/store/store.ts
@@ -0,0 +1,13 @@
+import { writable } from 'svelte/store'
+import { onPlayer } from './onPlayer'
+
+const S_onPlayer = writable("X")
+
+
+export { 
+
+	S_onPlayer,
+	// F_onPage,
+	// S_onPageData
+
+}
