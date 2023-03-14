a9984bba1ead1043848e45973c03e6abff3a5b74
G-yhlee <yhleesoft@gmail.com>
2023-03-14 14:32:30 +0900

    add board

--- src/game_x1/store/onHistory.ts
+++ src/game_x1/store/onHistory.ts
@@ -1,22 +1,42 @@
 import { match,P } from 'ts-pattern';
 import { get } from 'svelte/store'
 // import logger from '$lib/fucntion/logger';
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 import {S_onHistory,S_onBoard} from './store'
 
 
 
 export function onHistory(cell:any) {
     const prev_history = get(S_onHistory)
+    const prev_last = prev_history[prev_history.length-1]
+    const {stage,player} = prev_last
     const [x,y] = cell.xy
 
     S_onHistory.update(s=>s.concat({...cell,
-        stage: prev_history[prev_history.length-1].stage+1,
-        player: prev_history[prev_history.length-1].player=="X" ? "O" : "X"
+        stage: stage+1,
+        player: player=="X" ? "O" : "X"
     }))
+
+    console.log(get(S_onHistory));
+
+    const makeBoard = (history :any) => {
+        
+        // const genBoard = (x:number) => new Array(x).fill(new Array(x).fill("1,1")).map((arr,x)=>arr.map((_:any,y:any)=>({xy:[x+1,y+1], player: ""})))
+
+        // let map
+
+        // history.map(({player,xy} : any ) => {
+        //     let [x,y] = xy
+        //     genBoard(5)
+            
+        //     // return 
+        // })
+
+    }
+    
     
     S_onBoard.update(s=>{
-        s[x-1][y-1] = {...s[x-1][y-1], player: prev_history[prev_history.length-1].player == "X" ? "O" : "X"}
+        s[x-1][y-1] = {...s[x-1][y-1], player: player == "X" ? "O" : "X"}
         return s
     })
 
--- src/game_x1/store/store.ts
+++ src/game_x1/store/store.ts
@@ -6,16 +6,12 @@ import { updateBoard } from './updateBoard'
 import { updateStage } from './updateStage'
 import { get } from 'svelte/store'
 
-
-
-
 const genBoard = (x:number) => new Array(x).fill(new Array(x).fill("1,1")).map((arr,x)=>arr.map((_:any,y:any)=>({xy:[x+1,y+1], player: ""})))
-
 
 const S_onBoard = writable(genBoard(3))
 const S_onHistory = writable([
 	{xy: [0,0], player: "O", stage: 0},
-	    //     {xy: [2,2], player: "x", stage: 2},
+	    // {xy: [2,2], player: "x", stage: 2},
         // {xy: [2,3], player: "o", stage: 3},
         // {xy: [4,4], player: "x", stage: 4},
         // {xy: [2,4], player: "x", stage: 5},
@@ -23,17 +19,8 @@ const S_onHistory = writable([
 
 ])
 
-
-
-
-
-
-
-
 const S_onPlayer:Writable<string> = writable("X")
 const S_onStage = writable(0)
-
-
 
 export { 
 	S_onPlayer,
