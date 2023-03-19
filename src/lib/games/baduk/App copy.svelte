8dc6b9eec855459ce74982a9650bed6ac7abc072
G-yhlee <yhleesoft@gmail.com>
2023-03-19 23:23:02 +0900

    add go board

---
+++ src/lib/games/baduk/App copy.svelte
@@ -0,0 +1,19 @@
+<!-- <script>
+	let board = Array(19).fill(Array(19).fill(null));
+	let currentPlayer = 'black';
+  
+	function placeStone(row, col) {
+	  // Add logic to place stone and check for captures and illegal moves
+	  // Update the board and currentPlayer variables accordingly
+	}
+  </script>
+  
+  <table>
+	{#each board as row, rowIndex}
+	  <tr>
+		{#each row as cell, colIndex}
+		  <td on:click={() => placeStone(rowIndex, colIndex)}></td>
+		{/each}
+	  </tr>
+	{/each}
+  </table> -->
--- src/lib/games/baduk/App.svelte
+++ src/lib/games/baduk/App.svelte
@@ -1,33 +1,56 @@
 <script>
-	// @ts-nocheck
-	import Board from './components/board/Board.svelte';
-	import History from './components/history/History.svelte';
-	import Player from './components/player/player.svelte';
-	import Stage from './components/stage/stage.svelte';
-	import Winner from './components/winner/winner.svelte';
-	import Setting from './components/setting/setting.svelte';
+	let Dom_Stone ;
+	// $: Dom_Stone = document.getElementById("stones");
+  
+	function createStone(x, y) {
+	  console.log(x,y)
+	  const stone = document.createElement("div");
+	  stone.classList.add("stone");
+	  stone.style.left = 100+x * 25 + "px";
+	  stone.style.top = 10 + y * 25 + "px";
+	  stone.style.width = 20 + "px";
+	  stone.style.height = 20 + "px";
+	  stone.style.borderRadius = 50+"%";
+	  stone.style.backgroundColor= "black";
+	  stone.style.position= "absolute";
+	  stone.style.transform= "translate(-50%, -50%)";
+	  Dom_Stone.appendChild(stone);
+	}
+  </script>
+  
+  <button   class="pt-[10px] bg-slate-300 " id="board" bind:this={Dom_Stone}  on:click={(e) =>{
+	const x = Math.floor(e.offsetX / 25);
+	const y = Math.floor(e.offsetY / 25);
+	createStone(x, y);
+  }}>
 	
-</script>
+	<!-- <button bind:this={Dom_Stone} id="stones"></button> -->
+  </button>
+  
+  <button  class="pt-[10px] bg-slate-300 stone" ></button>
 
-<div class='game'>
-	<div class='game-board'>
-		<Board/>
-	</div>
-	<Winner></Winner>
-	<History></History>
-	<Setting></Setting>
-	
-</div>
 
 <style>
-	.game {
-		font: 14px "Century Gothic", Futura, sans-serif;
-		margin: 20px;
-		display: flex;
-		flex-direction: row;
-	}
 
-	ol {
-		padding-left: 30px;
-	}
+#board {
+  margin-top: 10px;
+  margin-left: 10px;
+  width: 500px;
+  height: 500px;
+  background-color: #ffffff;
+  display: grid;
+  grid-template-columns: repeat(19, 1fr);
+  grid-template-rows: repeat(19, 1fr);
+  gap: 1px;
+  border: 1px solid black;
+}
+.stone {
+  z-index: -10;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: black;
+  position: absolute;
+  transform: translate(-50%, -50%);
+}
 </style>
